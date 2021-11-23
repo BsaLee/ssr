fb0dc52f36d75ee2ed15baa1754d59a828ce22c1
BsaLee <50013416+BsaLee@users.noreply.github.com>
2021-11-23 09:19:14 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -7,7 +7,6 @@ export PATH
 #	Description: Install the ShadowsocksR server
 #	Version: 2.0.38
 #	Author: Toyo
-#	Blog: https://doub.io/ss-jc42/
 #=================================================
 
 sh_ver="2.0.38"
@@ -1451,7 +1450,7 @@ menu_status(){
 check_sys
 [[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] && [[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
 echo -e "  ShadowsocksR 一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-  ---- Toyo | doub.io/ss-jc42 ----
+
 
   ${Green_font_prefix}1.${Font_color_suffix} 安装 ShadowsocksR
   ${Green_font_prefix}2.${Font_color_suffix} 更新 ShadowsocksR
